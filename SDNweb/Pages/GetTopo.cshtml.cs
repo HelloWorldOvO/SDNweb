c5ad8be129fcd5cf95d4796751a0d68fc2a66e57
dockyu <dockyu@DESKTOP-CS-dockyu.localdomain>
2022-12-05 16:23:09 +0800

    first commit

---
+++ SDNweb/Pages/GetTopo.cshtml.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Globalization;
+using System.Net;
+using System.Reflection.Metadata.Ecma335;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace SDNweb.Pages
+{
+    public class GetTopoModel : PageModel
+    {
+        private readonly ILogger<GetTopoModel> _logger;
+
+        public GetTopoModel(ILogger<GetTopoModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            string dateTime = DateTime.Now.ToString("d", new CultureInfo("en-US"));
+            ViewData["TimeStamp"] = dateTime;
+        }
+
+        public void OnPostGetData()
+        {
+            String hosts = GetONOSInformation("hosts");
+            String Devices = GetONOSInformation("devices");
+            String Links = GetONOSInformation("links");
+            String Deviceids = DevicestoDevicesid(Devices);
+
+            ViewData["Devices"] = Devices;
+            ViewData["Hosts"] = hosts;
+            ViewData["Links"] = Links;
+            ViewData["Deviceids"] = Deviceids;
+
+
+            string dateTime = DateTime.Now.ToString("d", new CultureInfo("en-US"));
+            ViewData["TimeStamp"] = dateTime;
+
+            ViewData["Test"] = "OnPostGetTopo execute complete";
+        }
+
+        public String GetONOSInformation(String getItem)
+        {
+            HttpClientHandler httpClientHandler = new HttpClientHandler()
+            {
+                Credentials = new NetworkCredential("onos", "rocks"),
+            };
+
+            using (var client = new HttpClient(httpClientHandler))
+            {
+                var endpoint = new Uri("http://192.168.98.142:8181/onos/v1/" + getItem);
+                var result = client.GetAsync(endpoint).Result;
+                var jsonstring = result.Content.ReadAsStringAsync().Result;
+                return jsonstring;
+            }
+        }
+
+        public String DevicestoDevicesid(String devices)
+        {
+            List<string> deviceids = new List<string>();
+
+            foreach (var item in devices.Replace("}{", "}|{").Split('|'))
+            {
+                String line = item;
+                //Console.WriteLine(line);
+
+                dynamic dline = JObject.Parse(line);
+                String device = Convert.ToString(dline.devices[0]);
+                dynamic ddevice = JObject.Parse(device);
+                //Console.WriteLine("test:" + ddevice.id);
+                String deviceid = Convert.ToString(ddevice.id);
+                deviceids.Add(deviceid);
+            }
+
+
+            var result = String.Join(", ", deviceids.ToArray());
+            return result;
+        }
+
+
+    }
+}
---
+++ SDNweb/Pages/PostTest.cshtml.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Hosting;
+using System.Collections.Specialized;
+using System.Globalization;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using static System.Net.WebRequestMethods;
+using System.Text.Json.Serialization;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Text.Json.Nodes;
+
+
+namespace SDNweb.Pages
+{
+    public class PostTestModel : PageModel
+    {
+       
+        private readonly ILogger<PostTestModel> _logger;
+
+        public PostTestModel(ILogger<PostTestModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public class Criterion
+        {
+            public string type { get; set; }
+            public string ethType { get; set; }
+            public string ip { get; set; }
+        }
+        public class Instructions
+        {
+            public string type { get; set; }            
+            public int port { get; set; }
+        }
+
+        public void OnGet()
+        {           
+           string dateTime = DateTime.Now.ToString("d", new CultureInfo("en-US"));
+           ViewData["TimeStamp"] = dateTime;
+        }
+
+        public void OnPostPostTest(string WebIP,string deviceID, string appID, string stream)
+        {            
+            string dateTime = DateTime.Now.ToString("d", new CultureInfo("en-US"));            
+            ViewData["TimeStamp"] = dateTime;
+            POSTONOSInformation("d");
+        }
+
+        public async void POSTONOSInformation(String getItem)
+        {
+            List<string> deferred_ID = new List<string>() { };
+            List<Instructions> instructionsID = new List<Instructions>()
+            {
+                new Instructions(){type = "OUTPUT",port=1}                
+            };            
+
+            List<Criterion> criteria_ID = new List<Criterion>() 
+            {
+                new Criterion(){type = "ETH_TYPE",ethType = "0x0800"},
+                new Criterion(){type = "IPV4_DST",ip = "10.0.0.1/32"}
+            };
+            var senddata = new
+            {
+                priority = 4001,
+                timeout = 0,
+                isPermanent = true,
+                deviceId = "of:0000000000000001",
+                treatment = new
+                {
+                    deferred = deferred_ID,
+                    instructions = instructionsID
+                },
+                selector = new
+                {
+                    criteria = criteria_ID
+
+                }
+            };
+
+            HttpClientHandler httpClientHandler = new HttpClientHandler()
+            {
+                Credentials = new NetworkCredential("onos", "rocks"),
+            };
+
+            var json = JsonConvert.SerializeObject(senddata);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            var url = "http://192.168.98.142:8181/onos/v1/flows/of%3A0000000000000001?appId=hello";
+            using var client = new HttpClient(httpClientHandler);
+            var response = await client.PostAsync(url, data);
+            string result = response.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(result);
+        }
+    }
+}
